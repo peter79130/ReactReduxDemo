1efe1ba1363bf64f37326defd4272dc9654fd1ba
unknown <peter.fu@01FUP.wsmcorp.com>
2017-01-23 15:17:34 -0700

    React Redux Boilerplate Completed!

---
+++ README.md
@@ -0,0 +1,13 @@
+React-Redux boilerplate project
+
+Packages include:
+script: lodash, all the react and redux related libraries.
+style: sass, css
+
+Flow
+develop in /src
+JS/SASS entry: /src/index.js
+HTML /src/index.html is copied to /dist/index.html
+Images /src/images is copied to /dist/images
+npm start runs a webpack watch
+npm run build builds into /dist
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'lodash', 'react', 'react-dom', 'react-redux', 'redux', 'redux-form', 'redux-promise', 'redux-thunk'
+];
+
+const config = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+    publicPath: 'dist/'
+  },
+  module: {
+    loaders: [
+      {
+        loaders: 'babel-loader',
+        exclude: /node_modules/,
+        test: /\.js$/
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: ['css-loader', 'sass-loader']
+        }),
+        exclude: /node_modules/,
+        test: /\.scss$/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/style.[chunkhash].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor','manifest']
+    }),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
+
+module.exports = config;
